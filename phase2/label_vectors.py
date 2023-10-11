9d0e15b765a91aacbe4fa4e2728b20e4f14abc38
Charudatta Potdar <charudattapotdar@charus-mac.dhcp.asu.edu>
2023-10-11 12:59:39 -0700

    1. Changed pkl file names in label_vectors.py             2. Added functions for creating super descriptors using mean, standard deviation, min and max in label_vectors.py

--- phase2/label_vectors.py
+++ phase2/label_vectors.py
@@ -21,7 +21,6 @@ def label_fv_init(label: str, labelled_images: TypedDict, feature_space):
     for i in range(len(even_cur)):
         label_features.append(feature_space[even_cur[i]])
     label_features = np.asarray(label_features)
-
     return label_features
 
 def label_fv_kmediods(label: str, labelled_images: TypedDict, feature_shape):
@@ -36,10 +35,110 @@ def label_fv_kmediods(label: str, labelled_images: TypedDict, feature_shape):
             new_shape = (og_shape[0], np.prod(og_shape[1:]))
             label_features = label_features.reshape(new_shape)
         kmedoids = KMedoids(n_clusters=1).fit(label_features).cluster_centers_
-        # print(kmedoids.shape, type(kmedoids))
+        # print(len(kmedoids), kmedoids.shape, type(kmedoids))
         return kmedoids
     except KeyError:
         return None
+    
+
+
+
+
+#CODE BY CHARU STARTS
+
+#Mean
+def label_fv_mean(label: str, labelled_images: TypedDict, feature_space):
+    try:
+        label_features = label_fv_init(label, labelled_images, feature_space)
+        result = []
+        label_features_flattened = []
+        for i in label_features:
+            label_features_flattened.append(i.flatten())
+        
+        for i in range(len(label_features_flattened[0])):
+            column_array = []
+            for j in range(len(label_features_flattened)):
+                column_array.append(label_features_flattened[j][i])
+            column_array = np.array(column_array)
+            mean = column_array.mean()
+            result.append(mean)
+
+        result = np.array(result)
+        return result
+    except KeyError:
+        return None
+
+
+#Standard Deviation
+def label_fv_std(label: str, labelled_images: TypedDict, feature_space):
+    try:
+        label_features = label_fv_init(label, labelled_images, feature_space)
+        result = []
+        label_features_flattened = []
+        for i in label_features:
+            label_features_flattened.append(i.flatten())
+        
+        for i in range(len(label_features_flattened[0])):
+            column_array = []
+            for j in range(len(label_features_flattened)):
+                column_array.append(label_features_flattened[j][i])
+            column_array = np.array(column_array)
+            std = column_array.std()
+            result.append(std)
+
+        result = np.array(result)
+        return result
+    except KeyError:
+        return None
+    
+#Min
+def label_fv_min(label: str, labelled_images: TypedDict, feature_space):
+    try:
+        label_features = label_fv_init(label, labelled_images, feature_space)
+        result = []
+        label_features_flattened = []
+        for i in label_features:
+            label_features_flattened.append(i.flatten())
+        
+        for i in range(len(label_features_flattened[0])):
+            column_array = []
+            for j in range(len(label_features_flattened)):
+                column_array.append(label_features_flattened[j][i])
+            column_array = np.array(column_array)
+            min = column_array.min()
+            result.append(min)
+
+        result = np.array(result)
+        return result
+    except KeyError:
+        return None
+    
+#Max
+def label_fv_max(label: str, labelled_images: TypedDict, feature_space):
+    try:
+        label_features = label_fv_init(label, labelled_images, feature_space)
+        result = []
+        label_features_flattened = []
+        for i in label_features:
+            label_features_flattened.append(i.flatten())
+        
+        for i in range(len(label_features_flattened[0])):
+            column_array = []
+            for j in range(len(label_features_flattened)):
+                column_array.append(label_features_flattened[j][i])
+            column_array = np.array(column_array)
+            max = column_array.max()
+            result.append(max)
+
+        result = np.array(result)
+        return result
+    except KeyError:
+        return None
+    
+#CODE BY CHARU ENDS
+
+
+
 
 
 def label_image_distance_using_cosine(max_len: int, label_feature_vectors, dict_all_feature_vectors, k: int):
@@ -67,17 +166,23 @@ def create_labelled_feature_vectors(labelled_images):
     option = int_input()
     model_space = None
     match option:
-        case 1: model_space = torch.load('color_moments.pkl')
-        case 2: model_space = torch.load('hog.pkl') 
-        case 3: model_space = torch.load('resnet_layer3.pkl') 
-        case 4: model_space = torch.load('resnet_fc.pkl') 
-        case 5: model_space = torch.load('resnet_avgpool.pkl') 
+        case 1: model_space = torch.load('Color_moments_vectors.pkl')
+        case 2: model_space = torch.load('HOG_vectors.pkl') 
+        case 3: model_space = torch.load('layer3_vectors.pkl') 
+        case 4: model_space = torch.load('avgpool_vectors.pkl') 
+        case 5: model_space = torch.load('fc_layer_vectors.pkl') 
         case default: print('No matching input was selected')
     if model_space is not None:
         labelled_feature_vectors = {}
         for key in labelled_images.keys():
             # combine all feature vectors into one for label -- labelled embedding
             # add your min, mean, max code for label here
-            labelled_feature_vectors[key] = label_fv_kmediods(key, labelled_images, model_space)
+            # labelled_feature_vectors[key] = label_fv_kmediods(key, labelled_images, model_space)
+
+            # labelled_feature_vectors[key] = label_fv_mean(key, labelled_images, model_space)
+            # labelled_feature_vectors[key] = label_fv_std(key, labelled_images, model_space)
+            # labelled_feature_vectors[key] = label_fv_min(key, labelled_images, model_space)
+            labelled_feature_vectors[key] = label_fv_max(key, labelled_images, model_space)
+
         return (labelled_feature_vectors, model_space)
     else: return None
