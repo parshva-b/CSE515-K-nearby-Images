038b70ac3734827ba4adb832f36f5a077f646174
Gaurav Bansode <gauravbansode90@gmail.com>
2023-10-13 11:13:29 -0700

    Added some changes

--- phase2/Task9.py
+++ phase2/Task9.py
@@ -4,16 +4,20 @@
 import utils
 import label_vectors
 import os
+import numpy as np
+import torch
+import dimension_reduction
+from sklearn.metrics.pairwise import cosine_similarity
 
 class task9:
     def __init__(self) -> None:
         pass
     
     def menu2(self):
-        # Select a label
+        # # Select a label
         labels = utils.print_labels()
         label_num = utils.int_input()
-        selected_label = labels[label_num-1] # 0 index
+        selected_label = labels[label_num] # 0 index
         selected_label = selected_label.lower()
         print("\t selected label" , selected_label)
         
@@ -22,18 +26,22 @@ class task9:
 
         print(f"selected latent feature is = {latent_feature}")
         path = "./LatentSemantics/"+latent_feature
+
+        
+
+
         # show available files
         print("Available Latent Semantics : ")
         file_names = os.listdir(path)
         for i in range(len(file_names)):
             file_name = file_names[i]
             if file_name != ".gitkeep":
-                print(f"\t {i+1} : {file_name}")
+                print(f"\t {i} : {file_name}")
         
-        print("\n\t Select Latent Semantic")
+        # print("\n\t Select Latent Semantic")
         semantic_option = utils.int_input()
-        selected_latent_semantic = file_names[semantic_option-1]
-
+        selected_latent_semantic = file_names[semantic_option]
+        print("selected_latent_semantic ",  selected_latent_semantic)
         # find which feature space it is using
         feature_models = utils.feature_model
         feature_models_values = list(feature_models.values())
@@ -44,65 +52,43 @@ class task9:
             if feature_space in selected_latent:
                 detected_feature_space = feature_space
                 break  # Exit the loop as soon as a match is found
-        print(detected_feature_space)
+        print(f"detected feature space = {detected_feature_space}")
 
 
         ## we have detected_feature_space and selected_label
         ## We will now query all the feature vectors
         _, labelled_images = utils.initialise_project()
-        labelled_feature_vectors, _ = label_vectors.create_labelled_feature_vectors(labelled_images)
-        
-        print("\n\n Selected Labels Vector = #{}")
+        model_space = torch.load('fc_layer_vectors.pkl')
+        labels_feature_vector = label_vectors.label_fv_kmediods(selected_label, labelled_images, model_space)
+        # print(labels_feature_vector)
+        # find image similar to labels_feature_vector
+        similar_image = label_vectors.label_image_distance_using_cosine(len(model_space), labels_feature_vector, model_space, 1)
+        print(similar_image[0][0])
+        check_index = int(similar_image[0][0] / 2)
+        print(check_index)
+        latent_semantics = torch.load(path+"/"+selected_latent_semantic)
+        semantic_vector = latent_semantics[check_index]
 
-        return
+        similarities = cosine_similarity(latent_semantics, [semantic_vector])
+        distances = 1 - similarities
 
-    # def menu(self):
-        utils.print_labels()
-        # Add code to select Label here
+        sorted_indices = np.argsort(distances, axis=0).flatten()
+        print(sorted_indices)
+        labels_of_each_indices = []
+        image_labels = utils.get_image_categories()
+        for i in sorted_indices:
+            label = image_labels[i*2]
+            labels_of_each_indices.append(label)
 
-        ####
-
-        _, labelled_images = utils.initialise_project()
-        ## give latent space selection option
-        latent_feature = utils.get_user_input_latent_feature()
-        print(f"selected latent feature is = {latent_feature}")
-        path = "./LatentSemantics/"+latent_feature
-        print("Available Latent Semantics : ")
-        file_names = os.listdir(path)
-
-
-        for i in range(len(file_names)):
-            file_name = file_names[i]
-            if file_name != ".gitkeep":
-                print(f"{i+1} : {file_name}")
-
-        feature_models = utils.feature_model
-        feature_models_values = list(feature_models.values())
-        print(feature_models_values)
-
-        print('\n\nSelect Latent Semantic:')
-        selected_latent_semantic_num = utils.int_input()
-        selected_latent_semantic = file_names[selected_latent_semantic_num-1] # 0 indexing
-        selected_latent = selected_latent_semantic.strip().lower()
-        detected_feature_space = None
-
-        # Iterate through the feature spaces
-        for feature_space in feature_models_values:
-            feature_space = feature_space.strip().lower()
-            if feature_space in selected_latent:
-                detected_feature_space = feature_space
-                break  # Exit the loop as soon as a match is found
-
-        # Check if a feature space was detected
-        if detected_feature_space is not None:
-            print(f"Detected feature space: {detected_feature_space}")
-        else:
-            print("No feature space detected in the selected_latent string.")
-
-        labelled_feature_vectors, model_space = label_vectors.create_labelled_feature_vectors(labelled_images)
-        print(labelled_feature_vectors.keys())
-
-        # k = utils.get_user_input_k()
+        seen = set()
+        unique_elements = []
+        for item in labels_of_each_indices:
+            if item not in seen:
+                unique_elements.append(item)
+                seen.add(item)
+                if len(unique_elements) == 5:
+                    break
+        print(unique_elements)
         return
 
 if __name__ == "__main__":
