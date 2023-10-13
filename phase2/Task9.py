921881885df6812f4294872ebc9fb220d4ba1650
Gaurav Bansode <gauravbansode90@gmail.com>
2023-10-13 13:36:56 -0700

    Added code to get closests labels with image scores

--- phase2/Task9.py
+++ phase2/Task9.py
@@ -12,6 +12,8 @@ from Mongo.mongo_query_np import get_all_feature_descriptor_for_label, get_all_f
 
 class task9:
     def __init__(self) -> None:
+        self.labels = utils.get_labels()
+        self.image_labels = utils.get_image_categories()
         pass  
         
     def select_latent_semantics(self, path):
@@ -42,15 +44,27 @@ class task9:
 
         return selected_latent_semantic, detected_feature_space
 
-    def find_closest(self, latent_semantics, semantic_vector, k):
+    def find_closest_LS3(self, latent_semantics, semantic_vector, k):
+        similarities = cosine_similarity(latent_semantics, [semantic_vector])
+        distances = 1 - similarities
+
+        sorted_indices = np.argsort(distances, axis=0).flatten()
+        unique_elements = []
+        for i in range (0, len(sorted_indices)):
+            unique_elements.append((self.labels[i], similarities[i]))
+            if len(unique_elements) == k+1:
+                    break
+        print(unique_elements)
+        return
+
+    def find_closest_labels_with_image_reference(self, latent_semantics, semantic_vector, k):
         similarities = cosine_similarity(latent_semantics, [semantic_vector])
         distances = 1 - similarities
 
         sorted_indices = np.argsort(distances, axis=0).flatten()
         labels_of_each_indices = []
-        image_labels = utils.get_image_categories()
         for i in sorted_indices:
-            label = image_labels[i*2]
+            label = self.image_labels[i*2]
             labels_of_each_indices.append(label)
 
         seen = set()
@@ -81,7 +95,7 @@ class task9:
         latent_semantics = torch.load(path+"/"+selected_latent_semantic)
         semantic_vector = latent_semantics[closest_image_id]
 
-        self.find_closest(latent_semantics, semantic_vector, k)
+        self.find_closest_labels_with_image_reference(latent_semantics, semantic_vector, k)
         return
     
     def LS2(self,selected_label, path, k):
@@ -90,19 +104,41 @@ class task9:
     
     def LS3(self,selected_label, path, k):
         selected_latent_semantic, detected_feature_space = self.select_latent_semantics(path)
+        # cur_label_fv = label_vectors.label_fv_kmediods(selected_label, detected_feature_space)
+        index = self.labels.index(selected_label)
+        
+        latent_semantics = torch.load(path+"/"+selected_latent_semantic)
+        semantic_vector = latent_semantics[index]
+
+        self.find_closest_LS3(latent_semantics, semantic_vector, k)
         return
     
     def LS4(self,selected_label, path, k):
         selected_latent_semantic, detected_feature_space = self.select_latent_semantics(path)
+
+        # get particular labels vector
+        cur_label_fv = label_vectors.label_fv_kmediods(selected_label, detected_feature_space)
+
+        # load Entire Model Space
+        model_space = get_all_feature_descriptor(detected_feature_space)
+        
+        # find closest image
+        closest_image = label_vectors.label_image_distance_using_cosine(len(model_space), cur_label_fv, model_space, 1)
+        closest_image_id = closest_image[0][0]
+
+        latent_semantics = torch.load(path+"/"+selected_latent_semantic)
+        semantic_vector = latent_semantics[closest_image_id]
+
+        self.find_closest_labels_with_image_reference(latent_semantics, semantic_vector, k)
         return
 
-    def menu2(self):
+    def menu(self):
         # # Select a label
-        labels = utils.get_labels()
+        
         # labels = utils.print_labels()
         print("Input Label Number")
         label_num = utils.int_input()
-        selected_label = labels[label_num] # 0 index
+        selected_label = self.labels[label_num] # 0 index
         print("\t selected label" , selected_label)
         
         # Show latent_features and give option to select [LS1, LS2, LS3, etc.]
@@ -124,5 +160,4 @@ class task9:
 
 if __name__ == "__main__":
     temp = task9()
-    # temp.menu()
-    temp.menu2()
+    temp.menu()
